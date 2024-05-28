2e981f6de7069f220b8327f860ff8e44ca4c3d11
Matteo Tomatis <127085952+Matteo-Tomatis@users.noreply.github.com>
2024-05-28 17:43:42 +0200

    Update main.py

--- NLQ/VSLNet/main.py
+++ NLQ/VSLNet/main.py
@@ -158,13 +158,11 @@ def main(configs, parser):
                     word_ids, char_ids, vfeats, video_mask, query_mask
                 )
                 # compute loss
-                highlight_loss = model.compute_highlight_loss(
-                    h_score, h_labels, video_mask
-                )
+                highlight_loss = 0
                 loc_loss = model.compute_loss(
                     start_logits, end_logits, s_labels, e_labels
                 )
-                total_loss = loc_loss + configs.highlight_lambda * highlight_loss
+                total_loss = loc_loss
                 # compute and apply gradients
                 optimizer.zero_grad()
                 total_loss.backward()
