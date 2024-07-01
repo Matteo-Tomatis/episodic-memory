6e5577d774fdc6fe6ea7dffbb9cf83592e0d2cb6
Matteo Tomatis <127085952+Matteo-Tomatis@users.noreply.github.com>
2024-07-01 20:49:02 +0200

    Update VSLNet.py

--- NLQ/VSLNet/model/VSLNet.py
+++ NLQ/VSLNet/model/VSLNet.py
@@ -3,7 +3,6 @@
 import torch
 import torch.nn as nn
 from transformers import AdamW, get_linear_schedule_with_warmup
-from transformers import BertModel
 
 from model.layers import (
     Embedding,
@@ -14,7 +13,6 @@ from model.layers import (
     ConditionedPredictor,
     HighLightLayer,
     BertEmbedding,
-    BERTEncoder,
 )
 
 
@@ -60,8 +58,15 @@ class VSLNet(nn.Module):
             dim=configs.dim,
             drop_rate=configs.drop_rate,
         )
-        self.BERTEncoder = BERTEncoder()
-        self.feature_encoder = FeatureEncoder(
+        self.feature_encoder_text = FeatureEncoder(
+            dim=configs.dim,
+            num_heads=configs.num_heads,
+            kernel_size=7,
+            num_layers=4,
+            max_pos_len=configs.max_pos_len,
+            drop_rate=configs.drop_rate,
+        )
+        self.feature_encoder_video = FeatureEncoder(
             dim=configs.dim,
             num_heads=configs.num_heads,
             kernel_size=7,
@@ -126,8 +131,8 @@ class VSLNet(nn.Module):
         else:
             query_features = self.embedding_net(word_ids, char_ids)
 
-        query_features = self.BERTEncoder(query_features, attention_mask=q_mask)
-        video_features = self.feature_encoder(video_features, mask=v_mask)
+        query_features = self.feature_encoder_text(query_features, mask=q_mask)
+        video_features = self.feature_encoder_video(video_features, mask=v_mask)
         features = self.cq_attention(video_features, query_features, v_mask, q_mask)
         features = self.cq_concat(features, query_features, q_mask)
         h_score = self.highlight_layer(features, v_mask)
